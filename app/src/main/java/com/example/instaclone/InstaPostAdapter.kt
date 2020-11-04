a67e7e4071d51ea35db788e4ed0e21bdaeef7aa8
isackouyole <isackobuyole@gmail.comgit>
2020-11-05 02:49:26 +0300

    InstaPostsAdapter.kt

--- app/src/main/java/com/example/instaclone/InstaPostAdapter.kt
+++ app/src/main/java/com/example/instaclone/InstaPostAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.row_post_item.view.*
 
 class InstaPostAdapter(var instaPostList: List<InstaPost>): RecyclerView.Adapter<InstaPostAdapter.InstaPostViewHolder> (){
--- app/src/main/java/com/example/instaclone/StoriesAdapter.kt
+++ app/src/main/java/com/example/instaclone/StoriesAdapter.kt
@@ -5,10 +5,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.instaclone.StoriesAdapter.StoriesViewHolder
 import kotlinx.android.synthetic.main.row_stories_item.view.*
 
-class StoriesAdapter(var storiesList: List<Story>): Adapter<StoriesViewHolder>() {
+class StoriesAdapter(var storiesList: List<Story>): RecyclerView.Adapter<StoriesAdapter.StoriesViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesViewHolder {
